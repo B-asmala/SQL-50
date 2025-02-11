6dfea411c14af043fdb11cecd3c27677054c51a5
B-asmala <basmalabayoumi@gmail.com>
2025-02-11 21:03:45 +0200

    problem 2

---
+++ 584.Find_Customer_Referee.sql
@@ -0,0 +1,7 @@
+/*
+MySQL uses three-valued logic -- TRUE, FALSE and UNKNOWN.
+Anything compared to NULL evaluates to the third value: UNKNOWN.
+That “anything” includes NULL itself! That’s why MySQL provides the IS NULL and IS NOT NULL operators to specifically check for NULL.
+*/
+SELECT name from Customer
+WHERE referee_id <> 2 OR referee_id IS NULL;
